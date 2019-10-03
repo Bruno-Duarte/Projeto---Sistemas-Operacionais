90b47b3b94aec91ef272577fb6f6c0a13ad18586
Bruno Duarte <38738091+Bruno-Duarte@users.noreply.github.com>
2019-10-03 10:57:09 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -5,7 +5,6 @@ clients = []
 clients_lock = threading.Lock()
 
 class Server(object):
-
 	def __init__(self, capacity):
 		self.capacity = capacity
 
@@ -14,37 +13,34 @@ class Server(object):
 		try:
 			while True:
 				data = client_conn.recv(1024)
-
-				print(client_conn.getpeername(), 'conectado')
-
 				if not data:
 					print(client_conn.getpeername(), 'disconectou-se')
 					break
 				elif data[:-1] == b'done':
 					print('Esta avisando que imprimiu')
-					#client_conn.send(b'print')
 				elif data[:-1] == b'request':
-					print('Esta pedindo para imprimir')
+					print(client_conn.getpeername(), end='')
+					print(' esta pedindo para imprimir')
 					clients[0].send(b'print')
 				else:
 					print('Nao reconhecido')
-					print(data)
 		except Exception as erro:
 			print(erro)
 			client_conn.close()
 
 	def run(self):
-		with socket.socket() as s: 
-		    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+		with socket.socket() as s:
+		    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                                                        
 		    s.bind(('', 50007))
-		    s.listen(5)
+		    s.listen(10)
 		    while True:
-		        conexao, endereco = s.accept()
+		        connection, address = s.accept()
 		        with clients_lock:
-		        	clients.append(conexao)
+		        	clients.append(connection)
 
-		        print('Server conectado por', endereco)
-		        threading.Thread(target=self.handle_client, args=(conexao,)).start() 
-
-server = Server(10)
-server.run()
+		        print('Server conectado por', address)
+		        threading.Thread(target=self.handle_client, args=(connection,)).start()  
+	                                                       
+if __name__ == "__main__":
+	server = Server(10)
+	server.run()
