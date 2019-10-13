8d295f9abadbde10ad6b0eb3eef9d72ddf3f1a22
Bruno Duarte <38738091+Bruno-Duarte@users.noreply.github.com>
2019-10-12 21:37:32 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -4,7 +4,10 @@ from os import system
 clients = []
 clients_lock = threading.Lock()
 
+count = 0
+
 class Server(object):
+
 	def __init__(self, capacity):
 		self.capacity = capacity
 
@@ -16,9 +19,9 @@ class Server(object):
 				if not data:
 					print(client_conn.getpeername(), 'disconectou-se')
 					break
-				elif data[:-1] == b'done':
-					print('Esta avisando que imprimiu')
 				elif data[:-1] == b'request':
+					global count
+					count += 1
 					print(client_conn.getpeername(), end='')
 					print(' esta pedindo para imprimir')
 					clients[0].send(b'print')
@@ -29,18 +32,17 @@ class Server(object):
 			client_conn.close()
 
 	def run(self):
-		with socket.socket() as s:
-		    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                                                        
+		with socket.socket() as s: 
+		    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                          
 		    s.bind(('', 50007))
-		    s.listen(10)
-		    while True:
+		    s.listen(2)
+		    while True: 
 		        connection, address = s.accept()
 		        with clients_lock:
 		        	clients.append(connection)
-
 		        print('Server conectado por', address)
-		        threading.Thread(target=self.handle_client, args=(connection,)).start()  
-	                                                       
+		        threading.Thread(target=self.handle_client, args=(connection,)).start()
+	                                                                       
 if __name__ == "__main__":
-	server = Server(10)
+	server = Server(2)
 	server.run()
