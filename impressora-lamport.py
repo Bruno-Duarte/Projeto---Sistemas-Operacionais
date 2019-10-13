3129c3533553ffb20ebab15340be5eb0aa3abde1
Bruno Duarte <38738091+Bruno-Duarte@users.noreply.github.com>
2019-10-12 21:51:58 -0300

    Update impressora-lamport.py

--- impressora-lamport.py
+++ impressora-lamport.py
@@ -1,100 +1,99 @@
-import socket, select, sys, queue
+import socket, select, sys, queue, threading, queue
 from time import sleep
-from threading import Thread
-from datetime import datetime
+from random import randint as rand
 
-THREADS_AMOUNT = 10
 BUFFER_SIZE = 10
+
+lock = threading.Lock()
 
 count = 0
 
-threads = []
+class Printer(object):
 
-class Printer(Thread):
 	taking = [
-		False, False, False, False, False,
+		False, False, False, False, False, 
 		False, False, False, False, False
 	]
+
 	ticket = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-	def __init__(self, id):
-		self.id = id
-		self.buffer = queue.Queue(BUFFER_SIZE) 
-		Thread.__init__(self)
+	def __init__(self, name):
+		self.name = name
+		self.buffer = queue.Queue(BUFFER_SIZE) 	
+
+	def pre_protocol(self, i):
+		Printer.taking[i] = True
+		Printer.ticket[i] = max(Printer.ticket) + 1
+		Printer.taking[i] = False
+		for j in range(1, BUFFER_SIZE):
+			while True:
+				if not Printer.taking[j]:
+					break
+			while True:
+				if Printer.ticket[j] == 0 or test(Printer.ticket[i], i, Printer.ticket[j], j):
+					break
+		
+	def critical_section(self):
+		global count
+		count += 1
+		self.print_document()
+		count -= 1
+
+	def post_protocol(self, i):
+		Printer.ticket[i] = 0
 
 	def handle_server(self, data):
-		if data == b'print':
-			if count == 10:
-				return
-			else:
-				self.pre_protocol(b'print')
-				print('Numero de pedidos no buffer {}: '.format(count))
-			while True:
-				ans = self.print_document()
-				if ans == b'done':
-					self.pre_protocol(b'done')
-					break
-			print('Numero de pedidos no buffer {}: '.format(count))
-			if not data:
-				return
-
-	def run(self):
-		with socket.socket() as s:
-			s.connect(('', 50007))
-			while True:
-				io_list = [sys.stdin, s]
-				ready_to_read,ready_to_write,in_error = select.select(io_list , [], [])  
-				if s in ready_to_read: 
-					data = s.recv(1024)
-				Thread(target=self.handle_server, args=(data, )).start()
-        
-	def pre_protocol(self, msg):
-		for i in range(THREADS_AMOUNT):
-			Printer.taking[i] = True
-			Printer.ticket[i] = max(Printer.ticket)
-			Printer.taking[i] = False
-			for j in range(1, THREADS_AMOUNT):
-				while True:
-					if not Printer.taking[j]:
-						break
-				while True:
-					if Printer.ticket[j] == 0 or max(Printer.ticket[i], Printer.ticket[j]):
-						break
-			self.critical_section(msg)
-			Printer.ticket[i] = 0
+		if not data: # ex: caso o servidor se desligue, ou conexao perdida
 			return
-
-	def critical_section(self, msg):
-		global count
-		if msg == b'print':
-			count += 1
-			self.buffer.put(1)
-		if msg == b'done':
-			count -= 1
-			self.buffer.get()
+		elif data == b'print':
+			self.pre_protocol(0)
+			self.critical_section()
+			self.post_protocol(0)
 
 	def print_document(self):
-		sleep(10)
-		return b'done'
+		print('{} está esperando...'.format(threading.get_ident()))
+		t = rand(2, 10)
+		lock.acquire()
+		sleep(t)
+		lock.release()
+		print('{} imprimiu em {}s'.format(threading.get_ident(), t))
+
+	def run(self, buffer):
+		while True:
+			item = buffer.get()
+			if item is None:
+				break
+			item.start()
+			item.join()
+			buffer.task_done()
+
 
 def main():
-	create_threads()
-	start_threads()
-	join_threads()
+	global count
+	printer = Printer('Impressora')
+	with socket.socket() as s: 
+		s.connect(('', 50007))
+		while True:
+			io_list = [sys.stdin, s]
+			ready_to_read, ready_to_write, in_error = select.select(io_list , [], [])   
+			if s in ready_to_read: 
+				data = s.recv(1024)
 
-def create_threads():
-	for id in range(THREADS_AMOUNT):
-		new_thread = Printer(datetime.now())
-		new_thread.run()
-		threads.append(new_thread)
+				thread = threading.Thread(target=printer.handle_server, args=(data, ))
+				if count <= BUFFER_SIZE - 1:
+					printer.buffer.put(thread)
+				else:
+					print('Impressora ocupada')
+				threading.Thread(target=printer.run, args=(printer.buffer, )).start()
 
-def start_threads():
-	for thread in threads:
-		thread.start()
 
-def join_threads():
-	for thread in threads:
-		thread.join()
+def test(a, b, c, d):
+    if a < c:
+        return True
+    elif a == c and b < d:
+        return True
+    return False
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
 	main()
