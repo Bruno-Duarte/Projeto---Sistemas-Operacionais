b2cdcee7543d8117854aab64683f19449d9c6fb6
Bruno Duarte <38738091+Bruno-Duarte@users.noreply.github.com>
2019-10-14 13:17:59 -0300

    Update impressora-lamport.py

--- impressora-lamport.py
+++ impressora-lamport.py
@@ -1,62 +1,67 @@
-import socket, select, sys, queue, threading, queue
+import socket, select, sys, threading, queue
 from time import sleep
 from random import randint as rand
 
 BUFFER_SIZE = 10
+MAX_UNIV_COUNT = 1000
 
-lock = threading.Lock()
+doc_count = 0
+univ_count = 0
 
-count = 0
+buffer = queue.Queue(BUFFER_SIZE)
+
+taking = [
+	False, False, False, False, False, 
+	False, False, False, False, False
+]
+
+tickets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 class Printer(object):
 
-	taking = [
-		False, False, False, False, False, 
-		False, False, False, False, False
-	]
-
-	ticket = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
 	def __init__(self, name):
 		self.name = name
-		self.buffer = queue.Queue(BUFFER_SIZE) 	
+		self.__id = None
 
-	def pre_protocol(self, i):
-		Printer.taking[i] = True
-		Printer.ticket[i] = max(Printer.ticket) + 1
-		Printer.taking[i] = False
-		for j in range(1, BUFFER_SIZE):
-			while True:
-				if not Printer.taking[j]:
-					break
-			while True:
-				if Printer.ticket[j] == 0 or test(Printer.ticket[i], i, Printer.ticket[j], j):
-					break
-		
+	def set_id(self, id):
+		self.__id = id
+
+	def get_id(self):
+		return self.__id
+
+	def acquire(self, i):
+		taking[i] = True
+		tickets[i] = max(tickets) + 1
+		taking[i] = False
+		for j in range(BUFFER_SIZE):
+			while taking[j]:
+				pass
+			while tickets[j] != 0 and (tickets[j], j) < (tickets[i], i):
+				pass
+	    
 	def critical_section(self):
-		global count
-		count += 1
 		self.print_document()
-		count -= 1
 
-	def post_protocol(self, i):
-		Printer.ticket[i] = 0
+	def release(self, i):
+		tickets[i] = 0
 
 	def handle_server(self, data):
 		if not data: 
 			return
 		elif data == b'print':
-			self.pre_protocol(0)
+			global doc_count
+			doc_count += 1
+			print('O documento {} está na fila...'.format(threading.get_ident()))
+			id = self.get_id()
+			self.acquire(id)
 			self.critical_section()
-			self.post_protocol(0)
+			self.release(id)
+			doc_count -= 1
 
 	def print_document(self):
-		print('{} está esperando...'.format(threading.get_ident()))
 		t = rand(2, 10)
-		lock.acquire()
 		sleep(t)
-		lock.release()
-		print('{} imprimiu em {}s'.format(threading.get_ident(), t))
+		print('O documento {} imprimiu em {}s'.format(threading.get_ident(), t))
 
 	def run(self, buffer):
 		while True:
@@ -69,8 +74,13 @@ class Printer(object):
 
 
 def main():
-	global count
-	printer = Printer('Impressora')
+	global doc_count
+	global univ_count
+	docs = []
+	for i in range(BUFFER_SIZE):
+		name = 'D' + str(i)
+		doc = Printer(name)
+		docs.append(doc)
 	with socket.socket() as s: 
 		s.connect(('', 50007))
 		while True:
@@ -78,22 +88,20 @@ def main():
 			ready_to_read, ready_to_write, in_error = select.select(io_list , [], [])   
 			if s in ready_to_read: 
 				data = s.recv(1024)
-
-				thread = threading.Thread(target=printer.handle_server, args=(data, ))
-				if count <= BUFFER_SIZE - 1:
-					printer.buffer.put(thread)
+				id = univ_count % 10
+				univ_count += 1
+				if univ_count == MAX_UNIV_COUNT:
+					univ_count = 0
+				docs[id].set_id(id)
+				thread = threading.Thread(target=docs[id].handle_server, args=(data, ))
+				if doc_count <= BUFFER_SIZE - 1:
+					buffer.put(thread)
 				else:
-					print('Impressora ocupada')
-				threading.Thread(target=printer.run, args=(printer.buffer, )).start()
-
-
-def test(a, b, c, d):
-    if a < c:
-        return True
-    elif a == c and b < d:
-        return True
-    return False
+					print('Buffer cheio!')
+				for i in range(BUFFER_SIZE):
+					threading.Thread(target=docs[i].run, args=(buffer, )).start()
 
 
 if __name__ == '__main__':
 	main()
+          
